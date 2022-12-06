5285d371dfe5df01df172f1b2ff143d216e80454
rkterungwa16 <terungwakombol@gmail.com>
2022-12-06 16:35:08 +0100

    remove redundant imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtService } from '@nestjs/jwt';
 
 import { User, UserSchema } from '@user/schemas/user.schema';
 import { AuthService } from './auth.service';
 import { TokenService } from './token.service';
 import { AuthController } from './auth.controller';
 import { TokenModule } from './token.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
