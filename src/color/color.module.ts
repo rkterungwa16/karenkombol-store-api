3e9320b86712ea9cbcab85f55b6c25d4ccdf6a07
rkterungwa16 <terungwakombol@gmail.com>
2022-12-08 04:58:01 +0100

    include jwt and token service in color and company module

--- src/color/color.module.ts
+++ src/color/color.module.ts
@@ -3,9 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Color, ColorSchema } from './schema/color.schema';
 import { ColorService } from './color.service';
 import { ColorController } from './color.controller';
+import { TokenService } from '@auth/token.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  providers: [ColorService],
+  providers: [ColorService, TokenService, JwtService],
   controllers: [ColorController],
   imports: [
     MongooseModule.forFeature([{ name: Color.name, schema: ColorSchema }]),
