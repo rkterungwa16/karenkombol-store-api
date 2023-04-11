1b32ee9235d6081ce69b25421796d66ff596c563
rkterungwa16 <terungwakombol@gmail.com>
2023-04-11 02:34:41 +0100

    modify cloudinary service

--- src/lib/cloudinary/cloudinary.module.ts
+++ src/lib/cloudinary/cloudinary.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { CloudinaryService } from './cloudinary.service';
 import { ConfigureCloudinaryModule } from './configure.cloudinary.service';
 
 @Module({
   imports: [ConfigureCloudinaryModule.register()],
   exports: [ConfigureCloudinaryModule.register()],
+  // Register CloudinaryService, ConfigCloudinaryService and ConfigService
+  // ConfigService will be used by ConfigCloudinaryService.
+  // ConfigCloudinaryService will be used by CloudinaryService.
+  providers: [
+    CloudinaryService,
+    ...ConfigureCloudinaryModule.register().providers,
+  ],
 })
 export class CloudinaryModule {}
