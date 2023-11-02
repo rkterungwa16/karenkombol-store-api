04e30824dcff123cc577826d88f87b0b75d23f13
rkterungwa16 <terungwakombol@gmail.com>
2023-11-02 05:55:30 +0100

    seed data for shirt fits, length, sleeve length and collar

---
+++ src/seeder/data/shirt.ts
@@ -0,0 +1,173 @@
+export const shirtFits = [
+  {
+    name: 'classic',
+    description: `
+      Straight cut shirt with long sleeves at the cuffs, with a button placket and a rigid stand-up collar.
+    `,
+  },
+  {
+    name: 'slim',
+    description: `
+      Shirt that fits the body at the waist, close-fitting silhouette.
+    `,
+  },
+  {
+    name: 'loose',
+    description: `
+      Spacious shirt with wider sleeves and waist area than the classic model.
+    `,
+  },
+  {
+    name: 'oversize',
+    description: `
+      A loose straight cut shirt, seemingly 2-3 sizes larger, usually with offset shoulder seams.
+    `,
+  },
+  {
+    name: 'asymmetric',
+    description: `
+      Shirt with different length or design of the bottom line or shelves.
+    `,
+  },
+];
+
+export const shirtLength = [
+  {
+    name: 'short',
+    description: `
+    Shirt with the bottom line above the line of the hips.
+    `,
+  },
+  {
+    name: 'long',
+    description: `
+      The bottom line of the shirt goes well below the middle of the fly pants.
+    `,
+  },
+  {
+    name: 'standard',
+    description: `
+      Shirt whose bottom line reaches the middle of the fly of jeans or pants.
+    `,
+  },
+];
+
+export const sleeveLength = [
+  {
+    name: 'cap',
+    description: `
+      Vest-like shirts, with open shoulders.
+    `,
+  },
+  {
+    name: 'short',
+    description: `
+      A sleeve that covers the shoulder to the middle of the shoulder joint or slightly abov
+    `,
+  },
+  {
+    name: '3/4',
+    description: `
+     Three-quarter sleeve - a shortened sleeve that covers the hand to the middle of the forearm.
+    `,
+  },
+  {
+    name: 'long',
+    description: `
+      Full-length sleeve - covers the arm from the shoulder to the protruding thumb root or wrist.
+    `,
+  },
+];
+
+export const shirtColloar = [
+  {
+    name: 'classic',
+    description: `
+      Collar with a small stand and the distance between the top button and the top of the collar of 7 cm.
+    `,
+  },
+  {
+    name: 'crew-neck',
+    description: `
+    A crew neck is a type of shirt or sweater that has a round neckline and no collar and is often worn with other layers.
+    `,
+  },
+  {
+    name: 'english',
+    description: `
+      Folded collar of jacket type, consisting of a stand, a lapel and lapels.
+    `,
+  },
+  {
+    name: 'button-down',
+    description: `
+      Collar with a stand and bounce, which are attached to the top of the shirt with buttons.
+    `,
+  },
+  {
+    name: 'apash',
+    description: `
+      Wide, stand-up collar, wide open to the shoulders.
+    `,
+  },
+  {
+    name: 'italian',
+    description: `
+      The collar of the classic cut, but a large distance between the ends of the departures.
+    `,
+  },
+  {
+    name: 'mandarin',
+    description: `
+      A low, narrow stand-up collar with pointed or rounded ends and without offsets. Another name is mao.
+    `,
+  },
+  {
+    name: 'double-collar',
+    description: `
+      A lined, double-layered collar consisting of two collars of the same shape but different sizes.
+    `,
+  },
+  {
+    name: 'straight-collar',
+    description: `
+      Standing collar with a right angle between the edges.
+    `,
+  },
+  {
+    name: 'bertha',
+    description: `
+      Wide flat semi-circular collar.
+    `,
+  },
+  {
+    name: 'club',
+    description: `
+      Informal collar with rounded off ends.
+    `,
+  },
+  {
+    name: 'lined',
+    description: `
+      Legging collar without a stand, the spurs of which lie loosely on the shoulders or chest.
+    `,
+  },
+  {
+    name: 'peter pan',
+    description: `
+      Small collar with rounded ends.
+    `,
+  },
+  {
+    name: 'stand-up',
+    description: `
+      Collar with a small stand and lapels lying on the shoulders or chest towards the shoulders.
+    `,
+  },
+  {
+    name: 'peter pan',
+    description: `
+      Standing collar of medium height with no flaps.
+    `,
+  },
+];
