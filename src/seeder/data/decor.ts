ad27634e5172c77192355c7a0164140f7e520fd2
rkterungwa16 <terungwakombol@gmail.com>
2023-11-02 16:04:45 +0100

    add decore seed data

---
+++ src/seeder/data/decor.ts
@@ -0,0 +1,62 @@
+export const shirtDecor = [
+  {
+    name: 'sequins',
+    description: `
+      The small flakes are usually round-shaped, made of shiny, dense polyethylene.
+    `,
+  },
+  {
+    name: 'embroidery',
+    description: `
+      A hand- or machine-made pattern applied to an already finished garment with threads.
+      Classic shirts usually have monograms or abbreviations embroidered on them.
+      On the more informal models, any patterns.
+    `,
+  },
+  {
+    name: 'hood',
+    description: `
+      Attached to the collar area of the shirt, a part of the garment that imitates a headpiece.
+      Characteristic of sporty style shirts.
+    `,
+  },
+  {
+    name: 'lace',
+    description: `
+      Textile product without a base in the form of an openwork ornament. Used for decorating sleeves, collar, cuffs.
+      Less commonly used for the shelf and back.
+    `,
+  },
+  {
+    name: 'logo',
+    description: `
+      Embroidered or stitched emblem, often repeating the logo of the brand producing the shirt.
+      In custom made shirts, it can be anything at the customer's discretion.
+      Most often placed on the chest.
+    `,
+  },
+  {
+    name: 'application',
+    description: `
+      Sewn to the shirt part of another fabric with different images - faces, vegetal ornaments, abstract designs.
+    `,
+  },
+  {
+    name: 'painting',
+    description: `
+      The image applied to the shirt with indelible acrylics, paints.
+    `,
+  },
+  {
+    name: 'rhinestones',
+    description: `
+      Shimmering stones of different shapes and diameters, sewn or glued to the shirt.
+    `,
+  },
+  {
+    name: 'beads',
+    description: `
+      Embroidery patterns on fabric with beads of beads.
+    `,
+  },
+];
