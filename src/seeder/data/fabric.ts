aa66e79544cb26a7a468f3ec7869941f662435be
rkterungwa16 <terungwakombol@gmail.com>
2023-11-02 15:57:37 +0100

    add fabric seed data

---
+++ src/seeder/data/fabric.ts
@@ -0,0 +1,115 @@
+export const shirtFabrics = [
+  {
+    name: 'cotton',
+    description: `
+      It is a body-friendly material used in up to 90% of all shirts.
+      Often used in combination with synthetic fibers - elastane, lycra, polyester.
+    `,
+  },
+  {
+    name: 'linen',
+    description: `
+      Natural material, hygroscopic and hypoallergenic.
+      It has a tendency to crease and wrinkle and is difficult to iron.
+    `,
+  },
+  {
+    name: 'corduroy',
+    description: `
+      Textured, "voluminous" cotton fabric with pile, having longitudinal "rubs" in the form of stripes.
+    `,
+  },
+  {
+    name: 'leather',
+    description: `
+      Sleek, dense natural or man-made material. Fashionable,
+      but not practical variety of shirts - does not "breathe" and requires proper care.
+    `,
+  },
+  {
+    name: 'chiffon',
+    description: `
+      A lightweight, flowing, translucent cotton, synthetic or silk fabric.
+      Not used for traditional shirts. The exception is blouse shirts.
+    `,
+  },
+  {
+    name: 'baize',
+    description: `
+      Dense, soft wool or cotton blend fabric with a thick but short combed pile. Keeps you warm.
+    `,
+  },
+  {
+    name: 'denim',
+    description: `
+      Dense, tough, coarse twill weave fabric traditionally used for jeans.
+      Strong and durable, but requires care.
+    `,
+  },
+  {
+    name: 'silk',
+    description: `
+      Shiny, lightweight, smooth silkworm cocoon fabric.
+      The fabric requires a lot of care - it does not tolerate heat and can only be washed delicately.
+    `,
+    shirt_styles: [],
+    dress_styles: [],
+  },
+  {
+    name: 'flannel',
+    description: `
+      Flannel is a soft, warming fabric made of cotton, wool or a combination of both. It has single-sided and double-sided short pile.
+    `,
+  },
+  {
+    name: 'woolen',
+    description: `
+      Fabric made from recycled animal wool - sheep, goats, camels. Breathable, warming, not crumple.
+      Allow only hand washing.
+    `,
+  },
+  {
+    name: 'viscose',
+    description: `
+      Synthetic fabric made from wood pulp. Optimally warming, hygroscopic, but wears out quickly.
+    `,
+  },
+  {
+    name: 'jersey',
+    description: `
+      Plastic, soft material created by weaving yarns of natural or synthetic origin.
+      One of the most popular materials.
+    `,
+  },
+  {
+    name: 'satin',
+    description: `
+      Silk or blended fabric with a mixture of synthetic fibers, created by the method of satin weaving.
+      Lightweight, smooth, durable.
+    `,
+  },
+  {
+    name: 'suede',
+    description: `
+      Can be natural - made of leather, or artificial - made of a mixture of cotton and synthetics. Soft, dense, with velvety surface.
+      Not resistant to mechanical influences.
+    `,
+  },
+  {
+    name: 'micro-corduroy',
+    description: `
+      Corduroy fabric with lesser width of the stitching.
+    `,
+  },
+  {
+    name: 'oxford',
+    description: `
+      Specially woven cotton fabric with a rough surface.
+      Soft and matt, hygroscopic, easy to care for.
+    `,
+  },
+  {
+    name: 'Lace',
+    description: '',
+  },
+];
